1dbbf185d98299b63dc57af371ed3b66af60f5a9
rakib <rakibulislmmribd@gmail.com>
2024-01-31 01:29:55 +0600

    Add: Template of auth and tasks module with resources (controllers, services, dtos etc).

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, TasksModule],
 })
 export class AppModule {}

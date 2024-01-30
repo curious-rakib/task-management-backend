3631caabf091f93bd5b5e9b64be241864dbc4049
rakib <rakibulislmmribd@gmail.com>
2024-01-31 00:57:32 +0600

    Add: Open API documentations, CORS, set-prefix:api, env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Swagger config.
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager')
+    .setDescription('API for managing tasks in a task manager application.')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+
+  // Swagger document.
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // Enable CORS.
+  app.enableCors();
+
+  // Set '/api' prefix to all routes.
+  app.setGlobalPrefix('api');
+
+  // Start server with dynamic URL and PORT.
+  await app.listen(env.PROT || 3000, () => {
+    console.log(
+      `Server is running on port ${env.URI || 'http://localhost'}:${env.PROT || 3000}`,
+    );
+  });
 }
 bootstrap();
